a3b091e998d5864b05bd5674c6760c5ee7d531e7
A-k-A-s-H-1 <chinnapareddyakashreddy16@gmail.com>
2024-11-07 11:18:16 +0530

    first commit

---
+++ src/test/java/com/akash/Kafka_demo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akash.Kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
